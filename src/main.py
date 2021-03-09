7eb4997fa10e3c2fcb4713b4cae2bf4d8f0868ef
hexbreak <theownerproject@gmail.com>
2021-03-09 17:39:20 +0000

    jwt implementation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
@@ -15,6 +18,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'rolando-franky'
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -41,6 +46,29 @@ def get_all_users():
         raise APIException('You should not be seeing this. Check the route for errors.', status_code=400)
 
     return jsonify(response_body), 200
+
+# Provide a method to create access tokens. The create_jwt()
+# function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    username = params.get('username', None)
+    password = params.get('password', None)
+
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=username)}
+    return jsonify(ret), 200
 
 # Login & Update / id
 @app.route('/user/<int:user_id>', methods=['PUT', 'GET'])
@@ -476,6 +504,14 @@ def deltags_dislike(user_id, disliked_id):
     print("/ print test for /", response_body)
 
     return jsonify(response_body), 200
+
+# Protect a view with jwt_required, which requires a valid jwt
+# to be present in the headers.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    return jsonify({'hello_from': get_jwt_identity()}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
